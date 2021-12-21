744bbe7a2eaa7a69087c05d9f4db8bcd8c971207
tirgue <55982589+tirgue@users.noreply.github.com>
2021-12-21 18:12:26 +0100

    add/remove nine points points

--- frontend/src/views/Console/NinePointsConsole/NinePointsConsole.js
+++ frontend/src/views/Console/NinePointsConsole/NinePointsConsole.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ButtonPrimary } from '../../../components/components.index'
+import axios from 'axios'
 import './NinePointsConsole.scss'
 
 const NinePointsConsole = () => {
+
+    const [teams, setTeams] = useState({})
+
+    useEffect(() => {
+        axios.get('/api/state')
+            .then(response => {
+                const state = response.data
+                const ninePointsState = state.games.ninePoints
+                setTeams(ninePointsState.teams)
+            })
+    }, []);
+
+    const handleAddPoint = (teamName, points) => {
+        console.log(teamName, points);
+        axios.post('/api/nine-points/addPoint', {
+            teamName: teamName,
+            points: points
+        })
+            .then(response => {
+                const state = response.data
+                const ninePointsState = state.games.ninePoints
+                setTeams(ninePointsState.teams)
+            })
+    }
+
+    const handleRemovePoint = (teamName, points) => {
+        axios.post('/api/nine-points/removePoint', {
+            teamName: teamName,
+            points: points
+        })
+            .then(response => {
+                const state = response.data
+                const ninePointsState = state.games.ninePoints
+                setTeams(ninePointsState.teams)
+            })
+    }
+
     return (
         <div id="ninePointsConsole">
             <div className="teamControllers">
                 {
-                    [...Array(4)].map(_ =>
-                        <div className="teamController">
-                            <div className="teamName">L'equipe qui a un long nom</div>
-                            <div className="point">9    </div>
-                            <ButtonPrimary className="addPoint">+</ButtonPrimary>
-                            <ButtonPrimary className="removePoint">-</ButtonPrimary>
+                    Object.entries(teams).map(([teamName, { points }]) =>
+                        <div key={teamName} className="teamController">
+                            <div className="teamName">{teamName}</div>
+                            <div className="point">{points}</div>
+                            <ButtonPrimary className="addPoint" onClick={() => handleAddPoint(teamName, 1)}>+</ButtonPrimary>
+                            <ButtonPrimary className="removePoint" onClick={() => handleRemovePoint(teamName, 1)}>-</ButtonPrimary>
                         </div>
                     )
                 }
