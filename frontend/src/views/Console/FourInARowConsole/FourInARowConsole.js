1d951fa0674dbf033fc79328a892d4b8003bd74e
tirgue <55982589+tirgue@users.noreply.github.com>
2021-12-21 18:26:45 +0100

    add/remove four in a row points

--- frontend/src/views/Console/FourInARowConsole/FourInARowConsole.js
+++ frontend/src/views/Console/FourInARowConsole/FourInARowConsole.js
@@ -1,19 +1,56 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { ButtonPrimary } from '../../../components/components.index';
 import './FourInARowConsole.scss'
 
 const FourInARowConsole = () => {
+
+    const [teams, setTeams] = useState({})
+
+    useEffect(() => {
+        axios.get('/api/state')
+            .then(response => {
+                const state = response.data
+                const fourInARowState = state.games.fourInARow
+                setTeams(fourInARowState.teams)
+            })
+    }, []);
+
+    const handleAddPoint = (teamName, points) => {
+        console.log(teamName, points);
+        axios.post('/api/four-in-a-row/addPoint', {
+            teamName: teamName,
+            points: points
+        })
+            .then(response => {
+                const state = response.data
+                const fourInARowState = state.games.fourInARow
+                setTeams(fourInARowState.teams)
+            })
+    }
+
+    const handleResetPoint = (teamName) => {
+        axios.post('/api/four-in-a-row/resetPoint', {
+            teamName: teamName
+        })
+            .then(response => {
+                const state = response.data
+                const fourInARowState = state.games.fourInARow
+                setTeams(fourInARowState.teams)
+            })
+    }
+
     return (
         <div id="fourInARowConsole">
             <div className="teamControllers">
                 {
-                    [...Array(4)].map(_ =>
+                    Object.entries(teams).map(([teamName, { currentPoints, bestPoints }]) =>
                         <div className="teamController">
-                            <div className="teamName">L'equipe qui a un long nom</div>
-                            <div className="point">2</div>
-                            <div className="point">0</div>
-                            <ButtonPrimary className="addPoint">+</ButtonPrimary>
-                            <ButtonPrimary className="removePoint">0</ButtonPrimary>
+                            <div className="teamName">{teamName}</div>
+                            <div className="point">{bestPoints}</div>
+                            <div className="point">{currentPoints}</div>
+                            <ButtonPrimary className="addPoint" onClick={() => handleAddPoint(teamName, 1)}>+</ButtonPrimary>
+                            <ButtonPrimary className="removePoint" onClick={() => handleResetPoint(teamName)}>0</ButtonPrimary>
                         </div>
                     )
                 }
