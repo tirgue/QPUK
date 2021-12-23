964816ca59545fb38c7286f16996a501f2f0fa30
tirgue <55982589+tirgue@users.noreply.github.com>
2021-12-23 18:53:41 +0100

    face to face timer flow

--- frontend/src/views/Screen/FaceToFace/FaceToFace.js
+++ frontend/src/views/Screen/FaceToFace/FaceToFace.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './FaceToFace.scss'
 
 const FaceToFace = ({ state, finalists }) => {
+    const [timer, setTimer] = useState(state.timer)
+
     const leftTeam = state.teams[finalists[0]]
     const rightTeam = state.teams[finalists[1]]
+
+    useEffect(() => {
+        if (!timer.running) return
+        const clock = setTimeout(() => {
+            const newDate = new Date()
+            const diff = (newDate - new Date(timer.lastStop)) / 1000
+            setTimer({
+                ...timer,
+                value: timer.value - diff,
+                lastStop: newDate
+            })
+        }, 10)
+
+        return () => clearTimeout(clock)
+    });
+
+    useEffect(() => {
+        setTimer(state.timer)
+        console.log(timer);
+    }, [state]);
 
     const buildPointClassName = (point) => {
         let className = "finalPoint "
@@ -23,7 +45,14 @@ const FaceToFace = ({ state, finalists }) => {
                     [...Array(4)].map((_, index) => {
                         const i = 4 - index
                         const className = buildPointClassName(i)
-                        return <div className={className}>{i}</div>
+                        let percent = Math.min((100 * timer.value / 5) - (i - 1) * 100, 100)
+                        percent = Math.max(percent, 0)
+                        return <div
+                            key={i}
+                            className={className}
+                            style={{
+                                background: `linear-gradient(0deg, #f2911c ${percent}%, #012792 ${percent}%)`
+                            }}>{i}</div>
                     })
                 }
             </div>
